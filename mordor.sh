64d6c3c372b85fd66506e84aaa3f8867c8166763
ArchimedesPi <archimedespi3141@gmail.com>
2014-01-21 12:03:00 -0600

    More commits! Sorry I don't have good commit messages yet... Signed-off-by: ArchimedesPi <archimedespi3141@gmail.com>

--- mordor.sh
+++ mordor.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
+# This is Mordor, the simplistic package manager
+# See the docs/ folder for more info
+# You'll probably want to try Sauron first in most cases. He's the king! (;
+# Otherwise, ring one of the Balrogs up. They'll be happy to help!
+# If none of that works, just contact the *issue tracker*. It's even more evil than Sauron (;
+# Just joking. https://github.com/ArchimedesPi/mordor/issues
+# Feel free to do pull requests, I'm *very* likely to merge them (:
+# So fork, add your cool bell/whistle, pull request, and see that same bell/whistle upstream!
 
 # Prints stuff about filing an issue at Github
 file_issue() {
 	echo >&2 "If you deem this a big deal, *please file an issue* at"
 	echo >&2 "https://github.com/ArchimedesPi/mordor/issues"
+}
+
+# Print an INFO message
+infoz() {
+	message="[INFO]: $1"
+	echo message
+}
+
+# Print an ERROR message
+errorz() {
+	message="[ERROR]: $1"
+	echo >&2 message
+}
+
+#Print a WARNING message
+warningz() {
+	message="[WARNING]: $1"
+	echo message
 }
 
 # Everything-After cutter
@@ -36,7 +62,7 @@ cut_after() {
 			echo "$end"
 			;;
 		(*)
-			echo >&2 "Incorrect positional call to cut_after() !!!"
+			errorz "Incorrect positional call to cut_after() !!!"
 			file_issue
 			exit 1
 			;;
@@ -120,38 +146,71 @@ parse_url_handler() {
 		echo "balrog&$giturl^$package"
 		return
 	else
-  		echo >&2 "OOOPS! I don't recognize that!"
+  		errorz "OOOPS! I don't recognize that!"
 		file_issue
 	fi
+}
+
+# Check if a directory exists
+directory_exists() {
+	location="$1"
+	if [ -d location ]; then
+		echo true;
+	else
+		echo false;
+	fi
+}
+
+# Fancily named wrapper for directory_exists()
+check_balrog_installed() {
+	location="$1"
+	echo `directory_exists "$location"`
 }
 
 # Process a parsed URL
 # This is the meat of Mordor!
 process_parsed_url() {
+	infoz "Processing parsed URL..."
 	case $prefix in
 	("git")
+		infoz "Normal Git repo / Github gist"
 		git_url=`cut_after "$parsed_url" "&" "end"`
+		infoz "Fetching from Git ($git_url)"
 		location=`git_fetch "$git_url" "" ".mordor"`
+		infoz "Found Orcfile!"
 		install_package "$location" "Orcfile"
+		
 		;;
 	("balrog")
+		# Get everything after `balrog&`
 		postfix=`cut_after "$parsed_url" "&" "end"`
+		# Get the repo's URL
 		git_url=`echo $postfix | cut -d'^' -f 1`
+		# What package does the user want?
 		package=`echo $postfix | cut -d'^' -f 2`
+		# Check if the Balrog is already downloaded! Make sure it's not hiding/lurking around a corner (;
 		is_balrog_installed=`check_balrog_installed $git_url`
+		# Do different things if the Balrog's there or not.
 		if !is_balrog_installed; then
+			# Fetch our Balrog!
 			location=`git_fetch "$git_url" "" ".mordor/balrogs"`
 		else
+			# What's the Balrog's name?
 			balrog_name=`balrog_name "$git_url"`
+			# Where is it hiding?
 			location=".mordor/$balrog_name"
+			# Make sure the Balrog is up to date!
 			git_pull "$location"
 		fi
-		
+
+		# Where *is* that Balrog?
 		location="$location/$package"
+		# Aaaah. Let's install the package. Load an Orcfile!
 		install_package "$location" "Orcfile"
 		;;
 	(*)
-		echo >&2 "Uh-oh... There's been an internal error processing the parser prefix $prefix!"
+		# This is only called if someone makes a coding mistake! (Me most likely...)
+		errorz "Uh-oh... There's been an internal error processing the parser prefix $prefix!"
 		file_issue
 		;;
 	esac
@@ -160,12 +219,16 @@ process_parsed_url() {
 
 # The program!
 
+# Get our <url> option
 url="$1"
 
+# Parse it
 parsed_url=`parse_url_handler "$url"`
 
+# Get the prefix (everything before &)
 prefix=`echo $parsed_url | cut -d'&' -f 1`
 
+# OK, now process *that*...
 process_parsed_url parsed_url prefix
 
 		
