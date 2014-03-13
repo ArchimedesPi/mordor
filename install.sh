abe33e43269bf4d202d4257f71d2ed896773c6bc
ArchimedesPi <archimedespi3141@gmail.com>
2014-03-13 14:41:01 -0500

    Added messaging library Signed-off-by: ArchimedesPi <archimedespi3141@gmail.com>

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Install script for Mordor
+
+INSTALLPREFIX = '/opt/mordor'
+VARPREFIX = '/var/mordor'
+
+echo '=== Installing Mordor ==='
+echo
+echo
+echo "==> Installing to $INSTALLPREFIX"
+echo "==> Settings @ $VARPREFIX"
---
+++ util/messaging.sh
@@ -0,0 +1,13 @@
+# Thanks Homebrew for the "creative" names :)
+
+ohai() {
+	echo "$(tput setaf 2)$(tput bold)==> $(tput sgr0)$1"
+}
+
+opoo() {
+	echo "$(tput setaf 3)$(tput bold)==> $(tput sgr0)$1"
+}
+
+onoe() {
+	echo "$(tput setaf 1)$(tput bold)==> $(tput sgr0)$1"
+}
