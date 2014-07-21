fc0ede264b0a4f4895a3795eb06b5bde5d39e742
ArchimedesPi <archimedespi3141@gmail.com>
2014-07-20 20:59:02 -0500

    More comments

--- lib/mordor/cli.rb
+++ lib/mordor/cli.rb
@@ -7,17 +7,17 @@ module Mordor
       puts "I have been zapped <arrrgh!>"
     end
     
-    desc "status", "Status of a package"
+    desc "status PACKAGE", "Status of a package"
     def status(package)
       puts "This *would* have been the status of #{package}"
     end
     
-    desc "fetch", "Fetch a package's repo to the current directory"
+    desc "fetch PACKAGE", "Fetch a package's repo to the current directory"
     def fetch(package)
       puts "This *would* have fetched the package #{package}"
     end
     
-    desc "install", "Install a package"
+    desc "install PACKAGE", "Install a package"
     def install(package)
       puts "Would install the package #{package}"
     end
