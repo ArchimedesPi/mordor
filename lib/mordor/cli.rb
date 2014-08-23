a7a1fcd01e893b26d04864d0e4ee032f58c9e0f1
Liam M <archimedespi3141@gmail.com>
2014-08-23 13:35:40 -0500

    Goodbye, zap

--- lib/mordor/cli.rb
+++ lib/mordor/cli.rb
@@ -1,23 +1,22 @@
 require 'thor'
 require 'mordor'
+require 'term/ansicolor'
+
+include Term::ANSIColor
 
 module Mordor
   class CLI < Thor
-    desc "zap", "Remove Mordor from this computer"
-    def zap
-      Mordor.zap
-    end
-    
+
     desc "status PACKAGE", "Status of a package"
     def status(package)
       puts "This *would* have been the status of #{package}"
     end
-    
+
     desc "fetch PACKAGE", "Fetch a package's repo to the current directory"
     def fetch(package)
       puts "This *would* have fetched the package #{package}"
     end
-    
+
     desc "install PACKAGE", "Install a package"
     def install(package)
       puts "Would install the package #{package}"
--- mordor.gemspec
+++ mordor.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.6"
-  
+
   spec.add_dependency "thor"
+  spec.add_dependency "term-ansicolor"
 end
