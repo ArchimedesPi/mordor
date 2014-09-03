2073302c7681646f16ea39dce9e3e78d74f2058a
Liam M <archimedespi3141@gmail.com>
2014-09-03 15:49:32 -0500

    Tests won't pass without what they're testing ;)

--- lib/mordor/utilities/gist.rb
+++ lib/mordor/utilities/gist.rb
@@ -1,11 +1,9 @@
-module Mordor
-  class Gist
-    def self.git_url(gist_id)
-      "https://gist.github.com/#{gist_id}.git"
-    end
+class Gist
+  def self.git_url(gist_id)
+    "https://gist.github.com/#{gist_id}.git"
+  end
 
-    def self.web_url(gist_id)
-      "https://gist.github.com/#{gist_id}"
-    end
+  def self.web_url(gist_id)
+    "https://gist.github.com/#{gist_id}"
   end
 end
