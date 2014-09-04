674dad68a2fafea8bb880a6643ec48e4d5e76496
Liam M <archimedespi3141@gmail.com>
2014-09-04 14:41:48 -0500

    Fix all the docs

--- lib/mordor/utilities/gist.rb
+++ lib/mordor/utilities/gist.rb
@@ -1,15 +1,15 @@
 # "Global" class to host helper functions for doing Github Gist transactions
 class Gist
   # Converts a Gist ID to a Git over HTTPS url
-  # @param [String] the Gist ID
+  # @param gist_id [String] the Gist ID
   # @return [String] the Git over HTTPS url
   def self.git_url(gist_id)
     "https://gist.github.com/#{gist_id}.git"
   end
 
   # Converts a Gist ID to a url for the webpage of that Gist
-# @param [String] the Gist ID
-# @return [String] the webpage url of the Gist
+  # @param gist_id [String] the Gist ID
+  # @return [String] the webpage url of the Gist
   def self.web_url(gist_id)
     "https://gist.github.com/#{gist_id}"
   end
