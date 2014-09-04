033f25377a560027382f3514f1436f688d8ba510
Liam M <archimedespi3141@gmail.com>
2014-09-04 11:15:47 -0500

    Docs

--- lib/mordor/package.rb
+++ lib/mordor/package.rb
@@ -8,18 +8,22 @@ module Mordor
     end
 
     def fetch
+      # Fetch the package from the package's @url.
       @status[:fetched] = true
     end
 
     def purge
+      # Reverses .fetch. Purges the package's download from the local filesystem
       @status[:fetched] = false
     end
 
     def install
+      # Install a package (ie run the buildscripts)
       @status[:installed] = true
     end
 
     def remove
+      # Remove a package that's been installed (ie run the destroyscripts)
       @status[:installed] = false
     end
   end
